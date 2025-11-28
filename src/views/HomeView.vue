4e593b420d0395678b7f285ea3e20a346132927c
Pavin Butprom <lw95173@linecorp.com>
2025-11-28 13:23:06 +0700

    remove example template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    test
   </main>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
