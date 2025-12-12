c0e1b551318bf34a1e53886139ab6796f6eeb6eb
Pavin Butprom <vin@makewebbkk.com>
2025-12-13 01:19:37 +0700

    fix: liff init

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -15,16 +15,16 @@ export function makeHeader(key: string, value: string | null = ''): ISetHeader |
 
 export async function getAccessTokenNameCard(): Promise<ISetHeader | null> {
   const liffId1 = import.meta.env.VITE_APP_LIFF_ID || ''
-  const liffId2 = import.meta.env.VITE_APP_LIFF_ID_2 || ''
-  if (liff.isLoggedIn() && liff.id === liffId1) {
-    const accessToken = liff.getIDToken()
-    console.assert(!!accessToken, 'Name-Card AccessToken not found')
-    return makeHeader('x-id-token-vc', accessToken)
-  }
-  if (liff.id === liffId2) {
-    liff.openWindow({ url: `https://liff.line.me/${liffId1}` })
-    return null
-  }
+  // const liffId2 = import.meta.env.VITE_APP_LIFF_ID_2 || ''
+  // if (liff.isLoggedIn() && liff.id === liffId1) {
+  //   const accessToken = liff.getIDToken()
+  //   console.assert(!!accessToken, 'Name-Card AccessToken not found')
+  //   return makeHeader('x-id-token-vc', accessToken)
+  // }
+  // if (liff.id === liffId2) {
+  //   liff.openWindow({ url: `https://liff.line.me/${liffId1}` })
+  //   return null
+  // }
   await liff?.init({
     liffId: liffId1,
     withLoginOnExternalBrowser: true,
@@ -35,17 +35,17 @@ export async function getAccessTokenNameCard(): Promise<ISetHeader | null> {
 }
 
 export async function getAccessTokenSeatReservation(): Promise<ISetHeader | null> {
-  const liffId1 = import.meta.env.VITE_APP_LIFF_ID || ''
+  // const liffId1 = import.meta.env.VITE_APP_LIFF_ID || ''
   const liffId2 = import.meta.env.VITE_APP_LIFF_ID_2 || ''
-  if (liff.isLoggedIn() && liff.id === liffId2) {
-    const accessToken = liff.getIDToken()
-    console.assert(!!accessToken, 'Seat-reservation AccessToken not found')
-    return makeHeader('x-id-token-lt', accessToken)
-  }
-  if (liff.id === liffId1) {
-    liff.openWindow({ url: `https://liff.line.me/${liffId2}` })
-    return null
-  }
+  // if (liff.isLoggedIn() && liff.id === liffId2) {
+  //   const accessToken = liff.getIDToken()
+  //   console.assert(!!accessToken, 'Seat-reservation AccessToken not found')
+  //   return makeHeader('x-id-token-lt', accessToken)
+  // }
+  // if (liff.id === liffId1) {
+  //   liff.openWindow({ url: `https://liff.line.me/${liffId2}` })
+  //   return null
+  // }
   await liff?.init({
     liffId: liffId2,
     withLoginOnExternalBrowser: true,
