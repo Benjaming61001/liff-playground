4ee254222c5dd422e82f8012b28d34361b27ed62
Pavin Butprom <vin@makewebbkk.com>
2025-12-13 03:14:34 +0700

    fix: liff init

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -5,27 +5,40 @@ export interface ISetHeader {
   value: unknown
 }
 
+export async function getLocalToken(key: string): Promise<string | null> {
+  return localStorage.getItem(key)
+}
+
+export function saveToken(key: string, token: string) {
+  localStorage.setItem(key, token)
+}
+
+export function clearTokens() {
+  localStorage.removeItem('x-id-token-vc')
+  localStorage.removeItem('x-id-token-lt')
+}
+
 export function makeHeader(key: string, value: string | null = ''): ISetHeader | null {
   if (!value) return null
   return { key, value }
 }
 
-async function initAndGetIdToken(liffId: string): Promise<string | null> {
-  await liff?.init({ 
-    liffId,
-    withLoginOnExternalBrowser: true
-  })
+export async function initAndGetIdToken(liffId: string): Promise<string | null> {
+  await liff.init({ liffId })
+  if (!liff.isLoggedIn()) {
+    liff.login({ redirectUri: window.location.href })
+    return null
+  }
+
   return liff.getIDToken()
 }
 
 export async function getAccessTokenNameCard(): Promise<ISetHeader | null> {
-  const liffId = import.meta.env.VITE_APP_LIFF_ID || ''
-  const accessToken = await initAndGetIdToken(liffId)
+  const accessToken = await getLocalToken('x-id-token-vc')
   return makeHeader('x-id-token-vc', accessToken)
 }
 
 export async function getAccessTokenSeatReservation(): Promise<ISetHeader | null> {
-  const liffId = import.meta.env.VITE_APP_LIFF_ID_2 || ''
-  const accessToken = await initAndGetIdToken(liffId)
+  const accessToken = await getLocalToken('x-id-token-lt')
   return makeHeader('x-id-token-lt', accessToken)
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,68 @@
 <template>
   <main>
-    <div>
-      setAuthHeader
-    </div>
-    <div>
-      <button @click="setAuthHeader()">
-        setAuthHeader
-      </button>
-    </div>
-    <pre style="width: 100%; white-space: break-space; word-break: break-all;">
-      isLoading: {{ isLoading }}
-      nameCardToken: {{ nameCardToken || '-' }}
-      seatReservationToken: {{ seatReservationToken || '-' }}
-    </pre>
     <pre>
-      is Same id Token: {{ nameCardToken?.value === seatReservationToken?.value }}
+      nameCardToken: {{ nameCardToken }}
+      seatReservationToken: {{ seatReservationToken }}
     </pre>
   </main>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import {
   getAccessTokenNameCard,
   getAccessTokenSeatReservation,
+  getLocalToken,
+  initAndGetIdToken,
   type ISetHeader,
+  makeHeader,
+  saveToken,
 } from '@/utils/Auth'
+import liff from '@line/liff'
 
-const isLoading = ref<boolean>(false)
+const LIFF_ID_NAME_CARD = import.meta.env.VITE_APP_LIFF_ID || ''
+const LIFF_ID_SEAT = import.meta.env.VITE_APP_LIFF_ID_2 || ''
+
 const nameCardToken = ref<ISetHeader | null>(null)
 const seatReservationToken = ref<ISetHeader | null>(null)
+
 async function setAuthHeader (): Promise<void> {
-  isLoading.value = true
+  const localVc = await getLocalToken('x-id-token-vc')
+  const localLt = await getLocalToken('x-id-token-lt')
+
+  if (localVc && localLt) {
+    nameCardToken.value = makeHeader('x-id-token-vc', localVc)
+    seatReservationToken.value = makeHeader('x-id-token-lt', localLt)
+    return
+  }
+
+  if (!localVc) {
+    if (!window.location.href.includes('redirect-to=vc')) {
+      liff.openWindow({url: `https://https://liff.line.me/${LIFF_ID_NAME_CARD}?redirect-to=vc`})
+      return
+    }
+    const token = await initAndGetIdToken(LIFF_ID_NAME_CARD)
+    if (!token) return
+    saveToken('x-id-token-vc', token)
+  }
+
+  if (!localLt) {
+    if (!window.location.href.includes('redirect-to=lt')) {
+      liff.openWindow({url: `https://https://liff.line.me/${LIFF_ID_SEAT}?redirect-to=lt`})
+      return
+    }
+    const token = await initAndGetIdToken(LIFF_ID_SEAT)
+    if (!token) return
+    saveToken('x-id-token-lt', token)
+  }
+
   const authHeaderNameCard: ISetHeader | null = await getAccessTokenNameCard()
   const authHeaderSeatReservation: ISetHeader | null = await getAccessTokenSeatReservation()
   if (authHeaderNameCard) nameCardToken.value = authHeaderNameCard
   if (authHeaderSeatReservation) seatReservationToken.value = authHeaderSeatReservation
-  isLoading.value = false
 }
+
+onMounted(setAuthHeader)
 </script>
 
 <style scoped>
