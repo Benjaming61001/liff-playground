9979f457a5b8990d082bbc115f11a14875d034e4
Pavin Butprom <lw95173@linecorp.com>
2026-02-12 15:17:40 +0700

    fix: mirror verify flow

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,37 @@
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import liff from '@line/liff'
+
+const route = useRoute()
 
 async function open(): Promise<void> {
   await liff.ready
-  const url = 'https://liff.line.me/2008573781-8qRbpYwz'
-  liff?.openWindow({ url })
+  if (!route.query.login) {
+    if (liff?.isLoggedIn()) {
+      liff?.logout()
+    }
+    const baseUrl = window.location.origin
+    liff?.login({ redirectUri: `${baseUrl}?login=true` })
+    return
+  }
+  const profile = await liff?.getProfile()
+  const userId = profile?.userId || ''
+  const idToken = liff?.getIDToken() || ''
+
+  console.log({
+    userId,
+    idToken
+  })
+
+  if (liff.isInClient()) {
+    const url = 'https://liff.line.me/2008573781-8qRbpYwz'
+    liff?.openWindow({ url })
+  } else {
+    const baseUrl = window.location.origin
+    window.location.replace(`${baseUrl}/virtual-card`)
+  }
 }
 
 async function liffInit(): Promise<void> {
