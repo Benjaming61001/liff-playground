c42b11031f433ba6d51e1b2b5695485d3f7a0086
Pavin Butprom <vin@makewebbkk.com>
2026-04-27 17:17:06 +0700

    fix: liff

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,6 @@ import liff from '@line/liff'
 async function liffInit(): Promise<void> {
   const liffId = import.meta.env.VITE_APP_LIFF_ID || ''
   await liff.init({ liffId })
-  console.log(liff.isLoggedIn())
-  if (!liff.isLoggedIn()) {
-    liff.login()
-  }
 }
 
 onMounted(liffInit)
